5c0cca6a9695ac72a784c59850ba4cb3a36658ec
Mikolaj Hojko - Contractor <Mikolaj.Hojko@contractor.qiagen.com>
2020-06-13 13:13:17 +0200

    Initial commit - part 0

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using RogueSheep.Display;
+using SFML.Graphics;
+
+namespace BurglarOfBabylon
+{
+    internal static class Program
+    {
+        private static RenderWindow window;
+        private static GameDisplay mainDisplay;
+        private static void Main()
+        {
+            window = WindowBuilder.CreateWindow("Burglar of Babylon", 16 * 60, 16 * 60);
+            var mapTilemapConfiguration = new TilemapConfiguration(16, 16, 16, 16);
+            mainDisplay = new GameDisplay((60, 60), (0, 0), mapTilemapConfiguration);
+
+            while (window.IsOpen)
+            {
+                window.DispatchEvents();
+
+                window.Clear();
+
+                mainDisplay.Draw("Hello world!", (4, 4), Color.Cyan, Color.White);
+                mainDisplay.DrawToWindow(window);
+
+                window.Display();
+            }
+        }
+    }
+}
