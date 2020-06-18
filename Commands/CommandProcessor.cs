164b72990b7b45ee619582c0caabb9ed3ddfcf71
KarbonKitty <308lapua.magnum@gmail.com>
2020-06-18 20:00:08 +0200

    Add impassability and some walls - part 2

--- Commands/CommandProcessor.cs
+++ Commands/CommandProcessor.cs
@@ -6,11 +6,19 @@ namespace BurglarOfBabylon.Commands
     {
         public static bool ProcessCommand(Command command, GameState state) => command switch
         {
-            MoveCommand m => ProcessMoveCommand(m),
-            NullCommand n => true,
+            MoveCommand m => ProcessMoveCommand(m, state),
+            NullCommand _ => true,
             _ => throw new InvalidOperationException("This type of command is not yet handled")
         };
 
-        private static bool ProcessMoveCommand(MoveCommand moveCommand) => moveCommand.Originator!.Move(moveCommand.Vector);
+        private static bool ProcessMoveCommand(MoveCommand moveCommand, GameState state)
+        {
+            var newPosition = moveCommand.Originator!.Position + moveCommand.Vector;
+            if (state.CurrentMap.IsAvailableForMove(newPosition))
+            {
+                return moveCommand.Originator!.Move(moveCommand.Vector);
+            }
+            return false;
+        }
     }
 }
--- GameState.cs
+++ GameState.cs
@@ -19,10 +19,21 @@ namespace BurglarOfBabylon
                 new MapEntity("Random guard", (12, 12), new GameTile(CP437Glyph.CapitalG, RogueColor.DarkMagenta))
             };
 
+            var floor = new MapObject(new GameTile(CP437Glyph.SmallDot, RogueColor.Grey));
+            var wall = new MapObject(new GameTile(CP437Glyph.Hash, RogueColor.DarkGrey), passable: false, transparent: false);
+
             var mapObjects = new MapObject[GameConsts.MapWidth * GameConsts.MapHeight];
             for (var i = 0; i < mapObjects.Length; i++)
             {
-                mapObjects[i] = new MapObject(new GameTile(CP437Glyph.SmallDot, RogueColor.Grey));
+                if (i % GameConsts.MapWidth == 0 || i % GameConsts.MapWidth == GameConsts.MapWidth - 1
+                    || i / GameConsts.MapHeight == 0 || i / GameConsts.MapHeight == GameConsts.MapHeight - 1)
+                {
+                    mapObjects[i] = wall;
+                }
+                else
+                {
+                    mapObjects[i] = floor;
+                }
             }
 
             CurrentMap = new Map(mapObjects, actors);
