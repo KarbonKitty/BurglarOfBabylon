2613cca945e4caa337b2304573feb0c2462946d0
KarbonKitty <308lapua.magnum@gmail.com>
2020-06-22 19:46:12 +0200

    Add 8-way movement - part 4

--- InputHandler.cs
+++ InputHandler.cs
@@ -20,6 +20,16 @@ namespace BurglarOfBabylon
                 Keyboard.Key.Down => new MoveCommand(gameState.Player, (0, 1)),
                 Keyboard.Key.Left => new MoveCommand(gameState.Player, (-1, 0)),
                 Keyboard.Key.Right => new MoveCommand(gameState.Player, (1, 0)),
+
+                Keyboard.Key.Numpad7 => new MoveCommand(gameState.Player, (-1, -1)),
+                Keyboard.Key.Numpad8 => new MoveCommand(gameState.Player, (0, -1)),
+                Keyboard.Key.Numpad9 => new MoveCommand(gameState.Player, (1, -1)),
+                Keyboard.Key.Numpad4 => new MoveCommand(gameState.Player, (-1, 0)),
+                Keyboard.Key.Numpad6 => new MoveCommand(gameState.Player, (1, 0)),
+                Keyboard.Key.Numpad1 => new MoveCommand(gameState.Player, (-1, 1)),
+                Keyboard.Key.Numpad2 => new MoveCommand(gameState.Player, (0, 1)),
+                Keyboard.Key.Numpad3 => new MoveCommand(gameState.Player, (1, 1)),
+
                 _ => new NullCommand()
             };
 
